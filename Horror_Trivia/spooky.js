b8e360bd8c4f8230c9818b8124b9b3d8126144be
DallasLind <mcgraw.dallas@gmail.com>
2019-10-23 10:57:44 -0400

    Hmmmm

--- Horror_Trivia/spooky.js
+++ Horror_Trivia/spooky.js
@@ -1,72 +1,36 @@
 //All questions with their respective answers
-const questions = [
-		{
-		question: "Which movie was early career Joel Coen an assistant editor for?",
-		answers: ["Halloween 2", "The Evil Dead", "An American Werewolf in London"],
-		correctAnswer: 1	
-		},
-		
-		{
-		question: "True or False - Tony Todd in The Candyman put real bees in his mouth for the movie's climax",
-		answers: ["True", "False"],
-		correctAnswer: 0
-		},
-		
-		{
-		question: "How many days did it take to film the Blair Witch Project?",
-		answers: ["8 days", "14 days", "5 days"],
-		correctAnswer: 0
-		},
-		
-		{
-		question: "What kind of mask is Michael Meyer's mask in Halloween?",
-		answers: ["A Generic Mask", "A modified William Shatner mask", "A modified Leonard Nimoy mask"],
-		correctAnswer: 1
-		},
+function quizQuestion(question, answers, correctAnswer){
+	this.question = question;
+	this.answers = answers;
+	this.correctAnswer = correctAnswer;
+}
 
-		{
-		question: "Which show had a rejected episode that went on to become Final Destination?",
-		answers: ["The Twilight Zone", "Tales from the Crypt", "The X-Files"],
-		correctAnswer: 2
-		},
+const allQuestions = [
+		new quizQuestion("Which movie was early career Joel Coen an assistant editor for?",["Halloween 2", "The Evil Dead", "An American Werewolf in London"], 1),
+	
+		new quizQuestion("True or False - Tony Todd in The Candyman put real bees in his mouth for the movie's climax",["True", "False"], 0),
+		
+		new quizQuestion("How many days did it take to film the Blair Witch Project?", ["8 days", "14 days", "5 days"], 0),
 
-		{
-		question: "Which installment of the Friday the 13th franchise saw Jason in space?",
-		answers: ["Jason X", "Friday the 13th Part VI: Jason Lives", "Freddy vs. Jason"],
-		correctAnswer: 0
-		},
-	
-		{
-		question: "Where are the infamous Exorcist stairs located?",
-		answers: ["Los Angeles", "New York City", "Georgetown"],
-		correctAnswer: 2
-		},
+		new quizQuestion("What kind of mask is Michael Meyer's mask in Halloween?", ["A Generic Mask", "A modified William Shatner mask", "A modified Leonard Nimoy mask"], 1),
 
-		{
-		question: "What classic teen comedy did the Texas Chainsaw Massacre 2 poster spoof?",
-		answers: ["Ferris Bueller's Day Off", "Sixteen Candles", "The Breakfast Club"],
-		correctAnswer: 2
-		},
-	
-		{
-		question: "Where did House of 1000 Corpses' Captain Spaulding get his name from?",
-		answers: ["Trick question, it's an original name", "Groucho Marx character", "A famous vaudeville performer"],
-		correctAnswer: 1
-		},
-	
-		{
-		question: "Which of these movies were directed by a woman?",
-		answers: ["Carrie", "The Babadook", "Ginger Snaps"],
-		correctAnswer: 1
-		}
+		new quizQuestion("Which show had a rejected episode that went on to become Final Destination?", ["The Twilight Zone", "Tales from the Crypt", "The X-Files"], 2),
+		
+		new quizQuestion("Which installment of the Friday the 13th franchise saw Jason in space?", ["Jason X", "Friday the 13th Part VI: Jason Lives", "Freddy vs. Jason"], 0),
+		
+		new quizQuestion("Where are the infamous Exorcist stairs located?", ["Los Angeles", "New York City", "Georgetown"], 2),
+
+		new quizQuestion("What classic teen comedy did the Texas Chainsaw Massacre 2 poster spoof?", ["Ferris Bueller's Day Off", "Sixteen Candles", "The Breakfast Club"], 2),
+
+		new quizQuestion("Where did House of 1000 Corpses' Captain Spaulding get his name from?", ["Trick question, it's an original name", "Groucho Marx character", "A famous vaudeville performer"], 1),
+
+		new quizQuestion("Which of these movies were directed by a woman?", ["Carrie", "The Babadook", "Ginger Snaps"], 1)
 ];
 
-//Organize my variables for the function to create my Quiz
-let score = 0;
-const startScore = document.querySelector(".score");
-startScore.innerHTML = score;
 
-let index = 0;
+//Scoring Variables
+let currentQuestion = 0;
+let scoring = 0;
 
 //Music
 let myMusic = document.getElementById("music");
